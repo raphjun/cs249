06bef050468e6e2d353c6b99f6d3407f2719f38e
Jason Travis <infomaniac7@gmail.com>
2013-10-07 17:34:19 -0700

    add: CircList and unit tests
    CircList is a circularly linked list implementing the java.util.List
    interface. The implemented methods include contains, toArray, get, set,
    remove, isEmpty, add, and size.

---
+++ src/project2/CircList.java
@@ -0,0 +1,279 @@
+package project2;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * CircList implements a generic circularly linked list
+ * @author Jason Travis
+ */
+public class CircList<E> implements List {
+
+    private int size;
+    private Node first;
+
+    public CircList() {
+        this.size = 0;
+        this.first = null;
+    }
+
+    /**
+     * Returns the number of elements in the list
+     *
+     * @return the number of elements in the list
+     */
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * Returns true if the list contains no elements
+     *
+     * @return true if the list contains no elements
+     */
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    /**
+     * Returns true if the list contains the specified element
+     *
+     * @param o the element
+     * @return true if the list contains the specified element
+     */
+    @Override
+    public boolean contains(Object o) {
+        if (this.size == 0) {
+            return false;
+        }
+        Node ptr = this.first;
+        for (int i = 0; i < this.size; i++) {
+            if (ptr.getElement().equals(o)) {
+                return true;
+            }
+            ptr = ptr.getNext();
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Returns an array containing all the elements in the list
+     *
+     * @return an array containing all the elements in the list
+     */
+    @Override
+    public E[] toArray() {
+        Node ptr = this.first;
+        Object[] arr = new Object[this.size];
+        for (int i = 0; i < this.size; i++, ptr = ptr.getNext()) {
+            arr[i] = ptr.getElement();
+        }
+        return (E[]) arr;
+    }
+
+    @Override
+    public E[] toArray(Object[] ts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Appends the specified element to the end of the list
+     *
+     * @param e the element to append
+     * @return true
+     */
+    @Override
+    public boolean add(Object e) {
+        Node n = new Node(e);
+        if (this.size == 0) {
+            first = n;
+        } else {
+            Node last = getNode(this.size - 1);
+            last.setNext(n);
+            n.setNext(this.first);
+        }
+        size++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(int i, Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean removeAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean retainAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Returns the element at the specified position in the list
+     *
+     * @param i list index
+     * @return the element at the specified index
+     */
+    @Override
+    public E get(int i) {
+        Node n = getNode(i);
+        if (n == null) {
+            return null;
+        }
+        return (E) n.getElement();
+    }
+
+    /**
+     * Replaces the element at the given index with the given element
+     *
+     * @param i the list index
+     * @param e the element to replace the current element
+     * @return the element if successful, otherwise null
+     */
+    @Override
+    public E set(int i, Object e) {
+        Node n = getNode(i);
+        if (n == null) {
+            return null;
+        }
+        n.setElement(e);
+        return (E) e;
+    }
+
+    @Override
+    public void add(int i, Object e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Removes the element at the given index
+     *
+     * @param i index of the element to remove
+     * @return the element if it exists, otherwise null
+     */
+    @Override
+    public E remove(int i) {
+        if (i >= this.size || this.size == 0) {
+            // The node does not exist
+            return null;
+        }
+        // If removing the first element the previous node is the last node,
+        // otherwise, it is the node at the previous index
+        Node prev = getNode(i == 0 ? this.size - 1 : i - 1);
+        if (--this.size == 0) {
+            // This was the last node
+            first = null;
+        } else if (i == 0) {
+            // first is the node to remove, move the pointer to the next node
+            first = first.getNext();
+        }
+        // Remove the node
+        Node n = prev.getNext();
+        prev.setNext(n.getNext());
+        n.setNext(null);
+        return (E) n.getElement();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListIterator listIterator(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List subList(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Returns the node before the given node
+     *
+     * @param n a node in the list
+     * @return the node before the given node
+     */
+    private Node getNode(int index) {
+        if (index >= this.size || index < 0) {
+            // It does not exist
+            return null;
+        }
+        Node ptr = this.first;
+        // Move the pointer to the given index
+        for (int i = 0; i < index; i++) {
+            ptr = ptr.getNext();
+        }
+        return ptr;
+    }
+
+    private class Node<E> {
+
+        private E element;
+        private Node next;
+
+        public Node(E e) {
+            this.element = e;
+            next = this;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E e) {
+            this.element = e;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node n) {
+            this.next = n;
+        }
+    }
+}
---
+++ test/project2/CircListTest.java
@@ -0,0 +1,375 @@
+package project2;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jason Travis
+ */
+public class CircListTest {
+
+    public CircListTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of size method, of class CircList.
+     */
+    @Test
+    public void testSize() {
+        System.out.println("size");
+        Object e = new Object();
+        CircList instance = new CircList();
+        assertEquals("Size should be zero when instantiated", 0, instance.size());
+        instance.add(e);
+        assertEquals("Size should be one when an object is inserted", 1, instance.size());
+        instance.remove(0);
+        assertEquals("Size should be zero when the object is removed", 0, instance.size());
+        instance.remove(0);
+        assertEquals("Size remain zero when there is no object to remove", 0, instance.size());
+    }
+
+    /**
+     * Test of isEmpty method, of class CircList.
+     */
+    @Test
+    public void testIsEmpty() {
+        System.out.println("isEmpty");
+        CircList instance = new CircList();
+        assertTrue("It should be empty when instantiated", instance.isEmpty());
+        if (!instance.add(new Object())) {
+            fail("Unable to add object to list");
+        }
+        assertFalse("It should not be empty when an item is inserted", instance.isEmpty());
+    }
+
+    /**
+     * Test of contains method, of class CircList.
+     */
+    @Test
+    public void testContains() {
+        System.out.println("contains");
+        Object e1 = new Object(), e2 = new Object(), e3 = new Object();
+        CircList instance = new CircList();
+        assertFalse("It should return false when the list is instantiated", instance.contains(e1));
+        instance.add(e1); instance.add(e2); instance.add(e3);
+        assertFalse("It should return false when the list does not contain the object", instance.contains(new Object()));
+        assertTrue("It should return true when the first item is the object", instance.contains(e1));
+        assertTrue("It should return true when the middle item is the object", instance.contains(e2));
+        assertTrue("It should return true when the last item is the object", instance.contains(e3));
+    }
+
+    /**
+     * Test of iterator method, of class CircList.
+     */
+    @Test
+    public void testIterator() {
+        System.out.println("iterator");
+        CircList instance = new CircList();
+        Iterator expResult = null;
+        Iterator result = instance.iterator();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toArray method, of class CircList.
+     */
+    @Test
+    public void testToArray_0args() {
+        System.out.println("toArray");
+        CircList instance = new CircList();
+        assertArrayEquals("It should return an empty array when instantiated", new Object[0], instance.toArray());
+        Object e1 = new Object(), e2 = new Object(), e3 = new Object();
+        Object[] expResult = new Object[] {e1, e2, e3};
+        instance.add(e1); instance.add(e2); instance.add(e3);
+        assertArrayEquals("It should return an array containing all elements in order", expResult, instance.toArray());
+    }
+
+    /**
+     * Test of toArray method, of class CircList.
+     */
+    @Test
+    public void testToArray_ObjectArr() {
+        System.out.println("toArray");
+        Object[] ts = null;
+        CircList instance = new CircList();
+        Object[] expResult = null;
+        Object[] result = instance.toArray(ts);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of add method, of class CircList.
+     */
+    @Test
+    public void testAdd_Object() {
+        System.out.println("add");
+        Integer e1 = new Integer(1), e2 = new Integer(2), e3 = new Integer(3);
+        CircList instance = new CircList();
+        instance.add(e1);
+        assertEquals(1, instance.size());
+        instance.add(e2);
+        assertEquals(2, instance.size());
+        instance.add(e3);
+        assertEquals(3, instance.size());
+        assertEquals("It should return the first item", e1, instance.remove(0));
+        assertEquals("It should return the second item", e2, instance.remove(0));
+        assertEquals("It should return the third item", e3, instance.remove(0));
+    }
+
+    /**
+     * Test of remove method, of class CircList.
+     */
+    @Test
+    public void testRemove_Object() {
+        System.out.println("remove");
+        Object o = null;
+        CircList instance = new CircList();
+        boolean expResult = false;
+        boolean result = instance.remove(o);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of containsAll method, of class CircList.
+     */
+    @Test
+    public void testContainsAll() {
+        System.out.println("containsAll");
+        Collection clctn = null;
+        CircList instance = new CircList();
+        boolean expResult = false;
+        boolean result = instance.containsAll(clctn);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addAll method, of class CircList.
+     */
+    @Test
+    public void testAddAll_Collection() {
+        System.out.println("addAll");
+        Collection clctn = null;
+        CircList instance = new CircList();
+        boolean expResult = false;
+        boolean result = instance.addAll(clctn);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addAll method, of class CircList.
+     */
+    @Test
+    public void testAddAll_int_Collection() {
+        System.out.println("addAll");
+        int i = 0;
+        Collection clctn = null;
+        CircList instance = new CircList();
+        boolean expResult = false;
+        boolean result = instance.addAll(i, clctn);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removeAll method, of class CircList.
+     */
+    @Test
+    public void testRemoveAll() {
+        System.out.println("removeAll");
+        Collection clctn = null;
+        CircList instance = new CircList();
+        boolean expResult = false;
+        boolean result = instance.removeAll(clctn);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of retainAll method, of class CircList.
+     */
+    @Test
+    public void testRetainAll() {
+        System.out.println("retainAll");
+        Collection clctn = null;
+        CircList instance = new CircList();
+        boolean expResult = false;
+        boolean result = instance.retainAll(clctn);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of clear method, of class CircList.
+     */
+    @Test
+    public void testClear() {
+        System.out.println("clear");
+        CircList instance = new CircList();
+        instance.clear();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of get method, of class CircList.
+     */
+    @Test
+    public void testGet() {
+        System.out.println("get");
+        CircList instance = new CircList();
+        Object expResult = new Object();
+        instance.add(expResult);
+        assertNull("It should return null when the index is out of bounds", instance.get(1));
+        assertNull("It should return null when the index is negative", instance.get(-1));
+        assertEquals("It should return the object when it does exists", expResult, instance.get(0));
+    }
+
+    /**
+     * Test of set method, of class CircList.
+     */
+    @Test
+    public void testSet() {
+        System.out.println("set");
+        Object e = new Object();
+        CircList instance = new CircList();
+        assertNull("It should return null when the index is out of bounds", instance.set(0, new Object()));
+        assertNull("It should return null when the index is negative", instance.set(-1, new Object()));
+        instance.add(new Object());
+        instance.set(0, e);
+        assertEquals("It should replace the element at the given index with the given element", e, instance.get(0));
+    }
+
+    /**
+     * Test of add method, of class CircList.
+     */
+    @Test
+    public void testAdd_int_Object() {
+        System.out.println("add");
+        int i = 0;
+        Object e = null;
+        CircList instance = new CircList();
+        instance.add(i, e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of remove method, of class CircList.
+     */
+    @Test
+    public void testRemove_int() {
+        System.out.println("remove");
+        Object e1 = new Object(), e2 = new Object(), e3 = new Object();
+        CircList instance = new CircList();
+        assertNull("It should return null when the list is empty", instance.remove(0));
+        assertNull("It should not wraparound when the list is empty", instance.remove(-1));
+        instance.add(e1); instance.add(e2); instance.add(e3);
+        assertNull("It should return null when the index does not exist", instance.remove(3));
+        assertEquals("It should return the third element", e3, instance.remove(2));
+        assertEquals("It should return the second element", e2, instance.remove(1));
+        assertEquals("It should return the first element", e1, instance.remove(0));
+    }
+
+    /**
+     * Test of indexOf method, of class CircList.
+     */
+    @Test
+    public void testIndexOf() {
+        System.out.println("indexOf");
+        Object o = null;
+        CircList instance = new CircList();
+        int expResult = 0;
+        int result = instance.indexOf(o);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of lastIndexOf method, of class CircList.
+     */
+    @Test
+    public void testLastIndexOf() {
+        System.out.println("lastIndexOf");
+        Object o = null;
+        CircList instance = new CircList();
+        int expResult = 0;
+        int result = instance.lastIndexOf(o);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of listIterator method, of class CircList.
+     */
+    @Test
+    public void testListIterator_0args() {
+        System.out.println("listIterator");
+        CircList instance = new CircList();
+        ListIterator expResult = null;
+        ListIterator result = instance.listIterator();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of listIterator method, of class CircList.
+     */
+    @Test
+    public void testListIterator_int() {
+        System.out.println("listIterator");
+        int i = 0;
+        CircList instance = new CircList();
+        ListIterator expResult = null;
+        ListIterator result = instance.listIterator(i);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of subList method, of class CircList.
+     */
+    @Test
+    public void testSubList() {
+        System.out.println("subList");
+        int i = 0;
+        int i1 = 0;
+        CircList instance = new CircList();
+        List expResult = null;
+        List result = instance.subList(i, i1);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
