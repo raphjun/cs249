13157a4d972b4c842149a073df1bba4e2f849418
Jason Travis <infomaniac7@gmail.com>
2013-10-07 19:05:35 -0700

    update CircleGame to use CircList ADT

--- src/project2/CircleGame.java
+++ src/project2/CircleGame.java
@@ -1,73 +1,85 @@
 /**
- * The purpose of this project is to ensure that you understand how to work
- * with linked lists, and can use them to implement other ADTs (e.g., stack and queues) and solve
- * non-trivial problems.
- * 
+ * The purpose of this project is to ensure that you understand how to work with
+ * linked lists, and can use them to implement other ADTs (e.g., stack and
+ * queues) and solve non-trivial problems.
+ *
  * @author Jason Travis
  */
 package project2;
 
-import java.util.ListIterator;
-
-import project2.SinglyLinkedCircularList;
+import java.util.Iterator;
 
 /**
- * Suppose you entered a contest to be the last person remaining in a group. The way the game
- * works is that everyone forms a circle and individuals count off up to a certain number in
- * clockwise order. Once the number is reached that person is eliminated and the count begins at 1
- * with the next person in the circle.
- * 
- * Design and implement an application to help ensure you win the contest. Inputs are the number of
- * people in the circle, the number used for counting off, and the location where counting starts.
- * The output should be the location you should start in to be the last person remaining. You should
- * use a circular singly linked list. The output is the list of persons being eliminated, each person
- * should have a name representing their original position in the circle (e.g., person1, person2, etc.).
+ * Suppose you entered a contest to be the last person remaining in a group. The
+ * way the game works is that everyone forms a circle and individuals count off
+ * up to a certain number in clockwise order. Once the number is reached that
+ * person is eliminated and the count begins at 1 with the next person in the
+ * circle.
+ *
+ * Design and implement an application to help ensure you win the contest.
+ * Inputs are the number of people in the circle, the number used for counting
+ * off, and the location where counting starts. The output should be the
+ * location you should start in to be the last person remaining. You should use
+ * a circular singly linked list. The output is the list of persons being
+ * eliminated, each person should have a name representing their original
+ * position in the circle (e.g., person1, person2, etc.).
  */
 public class CircleGame {
-	private int count, start;
-	private SinglyLinkedCircularList<Integer> circle;
 
-	public static void main(String[] args) {
-		CircleGame circleGame = new CircleGame(9, 2, 1);
-		circleGame.play();
-	}
-	
-	/**
-	 * CircleGame constructor.
-	 * @param people the number of people in the circle
-	 * @param count the count at which the current person is eliminated
-	 * @param start the count starting position
-	 */
-	public CircleGame(int people, int count, int start) {
-		this.count = count;
-		this.start = start;
-		
-		// Create circle of people
-		circle = new SinglyLinkedCircularList<Integer>();
-		for(int i = 1; i <= people; i++) {
-			circle.addLast(i);
-		}
-	}
-	
-	/**
-	 * Iterate through the circle printing the position of the person removed when the count is reached.
-	 * Prints the position of the last person in the circle and returns their position.
-	 * @return the position of the last person in the circle
-	 */
-	public int play() {
-		ListIterator<Integer> iter = circle.listIterator(start);
-		Integer lastRemoved = -1;
-		while(!circle.isEmpty()) {
-			for(int i = 0; i < count; i++) {
-				lastRemoved = iter.next();
-			}
-			iter.remove();
-			if(!circle.isEmpty()) {
-				System.out.println("Person" + lastRemoved + " eliminated.");
-			} else {
-				System.out.println("Person" + lastRemoved + " won.");
-			}
-		}
-		return lastRemoved;
-	}
+    private int count, start;
+    private CircList<Integer> circle;
+
+    public static void main(String[] args) {
+        CircleGame circleGame = new CircleGame(9, 2, 1);
+        circleGame.play();
+        circleGame = new CircleGame(9, 3, 3);
+        circleGame.play();
+    }
+
+    /**
+     * CircleGame constructor.
+     *
+     * @param people the number of people in the circle
+     * @param count the count at which the current person is eliminated
+     * @param start the count starting position
+     */
+    public CircleGame(int people, int count, int start) {
+        this.count = count;
+        this.start = start;
+
+        // Create circle of people
+        circle = new CircList<>();
+        for (int i = 1; i <= people; i++) {
+            circle.add(i);
+        }
+    }
+
+    /**
+     * Iterate through the circle printing the position of the person removed
+     * when the count is reached. Prints the position of the last person in the
+     * circle and returns their position.
+     *
+     * @return the position of the last person in the circle
+     */
+    public int play() {
+        Iterator iter = circle.iterator();
+        Integer lastRemoved = -1;
+        // Shift iterator to the starting position
+        for (int i = 0; i < this.start; i++) {
+            iter.next();
+        }
+        System.out.println("\nStarting Circle Game with " + circle.size() + " people starting with position " + this.start + " and a count of " + this.count);
+        while (iter.hasNext()) {
+            for (int i = 0; i < this.count - 1; i++) {
+                lastRemoved = (Integer) iter.next();
+            }
+            iter.remove();
+            if (iter.hasNext()) {
+                System.out.println("Person" + lastRemoved + " eliminated.");
+            } else {
+                System.out.println("Person" + lastRemoved + " won.");
+            }
+        }
+        return lastRemoved;
+    }
 }
