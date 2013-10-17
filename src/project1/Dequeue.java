711aa09a4c45cfb6a5b1d89f6bd018393170c7f7
Jason Travis <infomaniac7@gmail.com>
2013-10-16 21:47:22 -0700

    add Dequeue toString method and set private variables to protected
    Set pointers and underlying array to protected so classes that extend
    the Dequeue can access them.

--- src/project1/Dequeue.java
+++ src/project1/Dequeue.java
@@ -7,8 +7,8 @@ package project1;
  */
 public class Dequeue<E> {
 
-    private Object[] dequeue;
-    private int start, end;
+    protected Object[] dequeue;
+    protected int start, end;
 
     public Dequeue(int capacity) {
         dequeue = new Object[capacity];
@@ -18,7 +18,7 @@ public class Dequeue<E> {
 
     /**
      * insertLeft inserts an element on the left-side of the queue
-     * 
+     *
      * @param e the element to insert
      * @return true if successful
      */
@@ -44,7 +44,7 @@ public class Dequeue<E> {
 
     /**
      * insertRight inserts elements on the right-side of the queue
-     * 
+     *
      * @param e the element to insert
      * @return true if successful
      */
@@ -70,7 +70,7 @@ public class Dequeue<E> {
 
     /**
      * removeLeft removes elements from the left-side of the queue
-     * 
+     *
      * @return the removed element
      */
     public E removeLeft() {
@@ -83,7 +83,7 @@ public class Dequeue<E> {
             // This was the last item, move the pointers off the queue
             start = -1;
             end = -1;
-        } else if(start == dequeue.length-1) {
+        } else if (start == dequeue.length - 1) {
             // If there's no space to the right, wrap around to the first index
             start = 0;
         } else {
@@ -95,11 +95,11 @@ public class Dequeue<E> {
 
     /**
      * removeRight removes elements from the right-side of the queue
-     * 
+     *
      * @return the removed element
      */
     public E removeRight() {
-       if (isEmpty()) {
+        if (isEmpty()) {
             return null;
         }
         E e = (E) dequeue[end];
@@ -108,9 +108,9 @@ public class Dequeue<E> {
             // This was the last item, move the pointers off the queue 
             start = -1;
             end = -1;
-        } else if(end == 0) {
+        } else if (end == 0) {
             // If there's no space to the left, wrap around to the last index
-            end = dequeue.length-1;
+            end = dequeue.length - 1;
         } else {
             // Move the pointer left
             end--;
@@ -120,7 +120,7 @@ public class Dequeue<E> {
 
     /**
      * isEmpty returns true if the queue is empty
-     * 
+     *
      * @return true if empty
      */
     public boolean isEmpty() {
@@ -129,10 +129,42 @@ public class Dequeue<E> {
 
     /**
      * isFull returns true if the queue is full
-     * 
+     *
      * @return true if full
      */
     public boolean isFull() {
         return (end == start - 1) || (start == 0 && end == dequeue.length - 1) || dequeue.length == 0;
+    }
+
+    /**
+     * toString returns the String representation of the Dequeue
+     *
+     * @return a String representation of the Dequeue
+     */
+    @Override
+    public String toString() {
+        // Call the toString method of each element, separate them with
+        // commas, and wraps the whole thing with square brackets
+        String s = "[";
+        if (this.end < this.start) {
+            // The queue wraps around
+            for (int i = this.start; i < this.dequeue.length; i++) {
+                s += dequeue[i].toString() + ", ";
+            }
+            for (int i = 0; i <= this.end; i++) {
+                s += dequeue[i].toString() + ", ";
+            }
+        } else if (!isEmpty()) {
+            // The queue doesn't wrap
+            for (int i = this.start; i <= this.end; i++) {
+                s += dequeue[i].toString() + ", ";
+            }
+        } else {
+            // The queue is empty, add two characters so the return substring,
+            // which trims the trailing characters, doesn't throw an
+            // IndexOutOfBoundsException
+            s += ", ";
+        }
+        return s.substring(0, s.length() - 2) + "]";
     }
 }
