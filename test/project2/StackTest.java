25e166a70f4495c1caa4ec8dc2c04e01f2b93893
Jason Travis <infomaniac7@gmail.com>
2013-10-16 22:30:35 -0700

    add: Demonstrate that the Stack is generic
    Duplicate StackTest push/pop test with a different Object type

--- test/project2/StackTest.java
+++ test/project2/StackTest.java
@@ -48,13 +48,29 @@ public class StackTest {
     }
 
     /**
-     * Test of pop method, of class Stack.
+     * Test of push/pop method, of class Stack.
      */
     @Test
     public void testPushPop() {
         System.out.println("pop");
-        Object e1 = new Object(), e2 = new Object(), e3 = new Object();
-        Stack instance = new Stack();
+        Integer e1 = new Integer(1), e2 = new Integer(2), e3 = new Integer(3);
+        Stack<Integer> instance = new Stack();
+        assertNull("It should return null after initialization", instance.pop());
+        instance.push(e1); instance.push(e2); instance.push(e3);
+        assertEquals("It should return the last item pushed", e3, instance.pop());
+        assertEquals("It should return the middle item pushed", e2, instance.pop());
+        assertEquals("It should return the first item pushed", e1, instance.pop());
+        assertNull("It should return null when empty", instance.pop());
+    }
+    
+    /**
+     * Test of push/pop method, of class Stack.
+     */
+    @Test
+    public void testPushPopWithADifferentObjectType() {
+        System.out.println("pop");
+        Double e1 = new Double(1), e2 = new Double(2), e3 = new Double(3);
+        Stack<Double> instance = new Stack();
         assertNull("It should return null after initialization", instance.pop());
         instance.push(e1); instance.push(e2); instance.push(e3);
         assertEquals("It should return the last item pushed", e3, instance.pop());
