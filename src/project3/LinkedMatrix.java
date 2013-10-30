f5e1f9e8ef360c1960ea753075e879d2c1bf0492
Jason Travis <infomaniac7@gmail.com>
2013-10-30 13:39:19 -0700

    update: LinkedMatrix supports display and fill
    The matrix does not initialize the nodes down links. Insert does not work.

--- src/project3/LinkedMatrix.java
+++ src/project3/LinkedMatrix.java
@@ -1,155 +1,169 @@
 /**
- * Create a two-dimensional linked list to represent a matrix. Each node (except those in the top
- * row and those in the left column) should be pointed to by the link directly above it and by the link
- * on its left. Thus, you will have to update the Node class to include two pointers (e.g., right and
- * down)
- * 
- * The size of matrix should be defined by the user when the program starts. The values stored at
- * each location of the matrix (i.e., node) should be initialized based on the position i,j. For example,
- * the first node in the top left corner should have the value 00, the 1st node in the 4th row should
- * have the value 03 (Note: counting should start at 0). You can assume you the largest matrix
- * would be 10 x 10.
+ * Create a two-dimensional linked list to represent a matrix. Each node (except
+ * those in the top row and those in the left column) should be pointed to by
+ * the link directly above it and by the link on its left. Thus, you will have
+ * to update the Node class to include two pointers (e.g., right and down)
  *
- * Your implementation should allow users to insert a specific value at a specific location, fill the
- * entire matrix with a given value, ask for the sum (which should provide both row and column
- * sums formatted at the ends of the rows and columns), and/or just display the matrix.
- * 
+ * The size of matrix should be defined by the user when the program starts. The
+ * values stored at each location of the matrix (i.e., node) should be
+ * initialized based on the position i,j. For example, the first node in the top
+ * left corner should have the value 00, the 1st node in the 4th row should have
+ * the value 03 (Note: counting should start at 0). You can assume you the
+ * largest matrix would be 10 x 10.
+ *
+ * Your implementation should allow users to insert a specific value at a
+ * specific location, fill the entire matrix with a given value, ask for the sum
+ * (which should provide both row and column sums formatted at the ends of the
+ * rows and columns), and/or just display the matrix.
+ *
  * @author Jason Travis
  */
-package project2;
+package project3;
 
 public class LinkedMatrix {
-	private Node first;
-	private int width, height;
-	
-	public static void main(String[] args) {
-		LinkedMatrix matrix = new LinkedMatrix(5, 2);
-		matrix.display(false);
-	}
-	
-	public LinkedMatrix(int width, int height) {
-		this.width = width;
-		this.height = height;
-		
-		first = new Node(0, 0, 0);
-		Node rowStart = first;
-		Node current = first;
-		
-		// Initialize columns
-		for(int row = 0; row < height; row++) {
-			// Initialize row
-			for(int column = 0; column < width - 1; column++) {
-				current.right = new Node(column + 1, row, 0);
-				current.down = new Node(column, row + 1, 0);
-				current = current.right;
-			}
-			// Move to the start of the next row
-			rowStart.down = new Node(0, row, 0);
-			rowStart = rowStart.down;
-			current = rowStart;
-		}
-		
-		// Move from the pre-matrix node to the first node of the matrix
-		//first = first.next;
-	}
-	
-	/**
-	 * Displays the matrix
-	 * @param withSums prints with row and column sums if true
-	 */
-	public void display(boolean withSums) {
-		// TODO format to width of largest sum and content
-		// System.out.printf("%5d", n);
-		//int[] rowSums = new int[width];
-		//int[] colSums = new int[height];
-		Node rowStart = first;
-		Node current = first;
-		for(int row = 0; row < height; row ++) {
-			for(int column = 0; column < width; column++) {
-				System.out.print(" " + current.x + current.y);
-				current = current.right;
-			}
-			System.out.println();
-			rowStart = rowStart.down;
-			current = rowStart;
-		}
-		//Node n = first.down.next;
-		//System.out.println("" + n.x + n.y);
-	}
-	
-	/**
-	 * Fills the entire matrix with a given value
-	 * @param value the value for the matrix nodes
-	 */
-	public void fill(int value) {
-		Node columnTop = first;
-		Node current = first;
-		// fill row
-		for(int column = 0; column < width; column++) {
-			// fill column
-			for(int row = 0; row < height; row++) {
-				current.value = value;
-				current = current.down;
-			}
-			// move to the next column
-			columnTop = columnTop.right;
-			current = columnTop;
-		}
-	}
-	
-	/**
-	 * Inserts the given value in the node with position 'x', 'y'
-	 * @param x the x coordinate of the node
-	 * @param y the y coordinate of the node
-	 * @param content the content for the node
-	 */
-	public void insert(int x, int y, int content) {
-		Node n = first;
-		// Move to the node x coordinate
-		for(int i = 0; i < x; i++) {
-			n = n.right;
-		}
-		// Move to the node y coordinate
-		for(int i = 0; i < y; i++) {
-			n = n.down;
-		}
-		// Insert the given value
-		n.value = content;
-	}
-	
-	// TODO Node class comments
-	private class Node {
-		private int value;
-		public int x = 0, y = 0;
-		private Node right, down;
-		
-		private Node(int x, int y, int content) {
-			this.x = x;
-			this.y = y;
-			this.value = content;
-		}
 
-		/**
-		 * 
-		 * @param n
-		 */
-		public void setNext(Node n) {
-			this.right = n;
-		}
+    private Node first;
+    private int xDim, yDim;
 
-		/**
-		 * @return the content
-		 */
-		private int getContent() {
-			return value;
-		}
-		
-		/**
-		 * @return the next node
-		 */
-		private Node getNext() {
-			return right;
-		}
-	}
+    public static void main(String[] args) {
+        LinkedMatrix matrix = new LinkedMatrix(5, 2);
+        matrix.display(3, true);
+        System.out.println();
+        matrix.fill(42);
+        matrix.display(3, true);
+        System.out.println();
+        matrix.insert(1, 1, 100);
+        matrix.display(3, false);
+    }
 
+    /**
+     * @param xDim size in the 'x' dimension
+     * @param yDim size in the 'y' dimension
+     */
+    public LinkedMatrix(int xDim, int yDim) {
+        this.xDim = xDim;
+        this.yDim = yDim;
+        // Initialize first to a pre-matrix node
+        this.first = new Node(0, 0);
+
+        // Initialize rows
+        Node rowStart = this.first, current;
+        for (int row = 0; row < this.yDim; row++) {
+            // Create a new row
+            rowStart = (rowStart.down = new Node(0, row));
+            // Move to the start of the row
+            current = rowStart;
+            // Initialize columns
+            for (int column = 0; column < this.xDim - 1; column++) {
+                current = (current.right = new Node(column + 1, row));
+            }
+        }
+        // Move from the pre-matrix node to the first node of the matrix
+        this.first = this.first.down;
+    }
+
+    /**
+     * Displays the matrix.
+     *
+     * @param pad the max number of zeros to pad the node elements
+     * @param withSums include row and column sums
+     */
+    public void display(int pad, boolean withSums) {
+        int[] rowSums = new int[this.yDim];
+        int[] colSums = new int[this.xDim];
+
+        Node rowStart = this.first, current;
+        for (int row = 0; row < this.yDim; row++) {
+            // Move to the start of the row
+            current = rowStart;
+            // Print and sum the row first element
+            System.out.print(String.format(" %0" + pad + "d", current.element));
+            rowSums[row] += current.element;
+            colSums[0] += current.element;
+            // Print and sum the row elements
+            // Sum the column elements
+            for (int column = 1; column < this.xDim; column++) {
+                current = current.right;
+                rowSums[row] += current.element;
+                colSums[column] += current.element;
+                System.out.print(String.format(" %0" + pad + "d", current.element));
+            }
+            if (withSums) {
+                // Print row sums
+                System.out.print(String.format(" |%0" + pad + "d", rowSums[row]));
+            }
+            // Move to the next row
+            rowStart = rowStart.down;
+            System.out.println();
+        }
+        if (withSums) {
+            // Create a divider the width of the element padding
+            String divider = " ";
+            for (int i = 0; i < pad; i++) {
+                divider += "_";
+            }
+            // Print the divider between the matrix and the sums
+            for (int column = 0; column < this.xDim; column++) {
+                System.out.print(divider);
+            }
+            System.out.println();
+            // Print the column sums
+            for (int n : colSums) {
+                System.out.print(String.format(" %0" + pad + "d", n));
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * Fills the entire matrix with a given value
+     *
+     * @param value the value for the matrix nodes
+     */
+    public void fill(int value) {
+        Node rowStart = this.first, current;
+        for (int row = 0; row < this.yDim; row++) {
+            // Move to the start of the row and set its value
+            current = rowStart;
+            current.element = value;
+            // Set the value of each element
+            for (int column = 0; column < this.xDim-1; column++) {
+                current = current.right;
+                current.element = value;
+            }
+            // Move to the next row
+            rowStart = rowStart.down;
+        }
+    }
+
+    /**
+     * Inserts the given value in the node with position 'x', 'y'
+     *
+     * @param x the x coordinate of the node
+     * @param y the y coordinate of the node
+     * @param content the content for the node
+     */
+    public void insert(int x, int y, int content) {
+        Node n = first;
+        // Move to the node x coordinate
+        for (int i = 0; i < x-1; i++) {
+            n = n.right;
+        }
+        // Move to the node y coordinate
+        for (int i = 0; i < y-1; i++) {
+            n = n.down;
+        }
+        // Insert the given value
+        n.element = content;
+    }
+
+    private class Node {
+
+        private int element;
+        private Node right, down;
+
+        private Node(int x, int y) {
+            this.element = y * LinkedMatrix.this.xDim + x;
+        }
+    }
 }
